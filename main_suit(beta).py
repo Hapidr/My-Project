d324068d761aaebf94d5925e8dbb845c01f2ec67
Hapidr <67416357+Hapidr@users.noreply.github.com>
2021-12-04 20:18:11 +0700

    Update main_suit(beta).py
    added play again or exit option

--- main_suit(beta).py
+++ main_suit(beta).py
@@ -1,13 +1,16 @@
 import random
+while True:
+    player = input('Masukkan pilihan Anda (Batu, Gunting, Kertas) : ')
+    if player != "Gunting" and player != "gunting" and player != "Kertas" and player != "kertas" and player != "Batu" and player != "batu":
+        print("Masukkan huruf yang sesuai!")
 
-player = input('Masukkan pilihan Anda (Batu, Gunting, Kertas) : ')
-if player != "Gunting" and player != "Kertas" and player != "Batu": print("Masukkan huruf yang sesuai!")
+    else:
+        com = random.choice(["Kertas", "Batu", "Gunting"])
+        print(f"Anda memilih {player} Komputer memilih {com}")
 
-else:
-    com = random.choice(["Kertas", "Batu", "Gunting"])
-    print(f"Anda memilih {player} Komputer memilih {com}")
-
-    if (player == "Kertas" and com == "Batu") or (player == "Batu" and com == "Gunting") or (player == "Gunting" and com == "Kertas"):
-        print("Anda Menang!")
-    elif player == com: print("Hasilnya Seri")
-    else: print("Anda Kalah")
+        if (player == "Kertas" and com == "Batu") or (player == "Batu" and com == "Gunting") or (player == "Gunting" and com == "Kertas"):
+            print("Anda Menang!")
+        elif player == com: print("Hasilnya Seri")
+        else: print("Anda Kalah")
+    play = input("Ingin main lagi?\n Ya atau Tidak : ")
+    if play == "Tidak" or play == "tidak": print("Terima Kasih sudah bermain"), exit()
