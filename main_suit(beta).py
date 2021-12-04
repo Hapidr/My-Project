16518e1d6242be3b6e01f9be45e221402935fea4
Hapidr <67416357+Hapidr@users.noreply.github.com>
2021-12-04 21:36:19 +0700

    Update main_suit(beta).py

--- main_suit(beta).py
+++ main_suit(beta).py
@@ -1,4 +1,6 @@
 import random
+
+print("=====PERMAINAN BATU GUNTING KERTAS=====\n")
 while True:
     player = input('Masukkan pilihan Anda (Batu, Gunting, Kertas) : ')
     if player != "Gunting" and player != "gunting" and player != "Kertas" and player != "kertas" and player != "Batu" and player != "batu":
@@ -8,9 +10,12 @@ while True:
         com = random.choice(["Kertas", "Batu", "Gunting"])
         print(f"Anda memilih {player} Komputer memilih {com}")
 
-        if (player == "Kertas" and com == "Batu") or (player == "Batu" and com == "Gunting") or (player == "Gunting" and com == "Kertas"):
+        if (player == "Kertas" or player == "kertas" and com == "Batu") or (player == "Batu" or player == 'batu' and com == "Gunting") or (player == "Gunting" or player == "gunting" and com == "Kertas"):
             print("Anda Menang!")
-        elif player == com: print("Hasilnya Seri")
+        elif player == com: print("Seri!")
+        elif (player == "kertas" and com == "Kertas") or (player == 'batu' and com == "Batu") or (player == "gunting" and com == "Gunting"): print("Seri!")
         else: print("Anda Kalah")
-    play = input("Ingin main lagi?\n Ya atau Tidak : ")
-    if play == "Tidak" or play == "tidak": print("Terima Kasih sudah bermain"), exit()
+
+    play = input("\nIngin main lagi?\nYa / Tidak: ")
+    if play == "Tidak" or play == "tidak": print("Terima Kasih sudah bermain!"), exit()
+    elif play != "Ya" and play != "ya" and play != "Iya" and play != "iya": print("Pilih sesuai pilihan yang tersedia!"), exit()
