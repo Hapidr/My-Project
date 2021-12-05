71bca0d9952807e74a8db219b76f818d033bfe9d
Hapidr <67416357+Hapidr@users.noreply.github.com>
2021-12-05 08:28:36 +0700

    Update main_suit(beta).py
    Simplified

--- main_suit(beta).py
+++ main_suit(beta).py
@@ -3,19 +3,18 @@ import random
 print("=====PERMAINAN BATU GUNTING KERTAS=====\n")
 while True:
     player = input('Masukkan pilihan Anda (Batu, Gunting, Kertas) : ')
-    if player != "Gunting" and player != "gunting" and player != "Kertas" and player != "kertas" and player != "Batu" and player != "batu":
+    if player.lower() != "gunting" and player.lower() != "kertas" and player.lower() != "batu":
         print("Masukkan huruf yang sesuai!")
 
     else:
         com = random.choice(["Kertas", "Batu", "Gunting"])
-        print(f"Anda memilih {player} Komputer memilih {com}")
+        print(f"Anda memilih {player.upper()} Komputer memilih {com.upper()}")
 
-        if (player == "Kertas" or player == "kertas" and com == "Batu") or (player == "Batu" or player == 'batu' and com == "Gunting") or (player == "Gunting" or player == "gunting" and com == "Kertas"):
+        if (player.lower() == "kertas" and com == "Batu") or (player.lower() == "batu" and com == "Gunting") or (player.lower() == "gunting" and com == "Kertas"):
             print("Anda Menang!")
-        elif player == com: print("Seri!")
-        elif (player == "kertas" and com == "Kertas") or (player == 'batu' and com == "Batu") or (player == "gunting" and com == "Gunting"): print("Seri!")
+        elif player.lower() == com.lower(): print("Seri!")
         else: print("Anda Kalah")
 
     play = input("\nIngin main lagi?\nYa / Tidak: ")
-    if play == "Tidak" or play == "tidak": print("Terima Kasih sudah bermain!"), exit()
-    elif play != "Ya" and play != "ya" and play != "Iya" and play != "iya": print("Pilih sesuai pilihan yang tersedia!"), exit()
+    if play.lower() == "tidak": print("Terima kasih sudah bermain!"), exit()
+    elif play.lower() != "ya": print("Masukkan pilihan yang sesuai!"), exit()
