4f305fad0849abf5b4ca7b9fcd7efb2d1b44f7c2
Hapidr <67416357+Hapidr@users.noreply.github.com>
2022-01-30 08:50:42 +0700

    Update main_suit(beta).py
    - Give short name for Random module
    - Center print in line 3
    - Delete some space

--- main_suit(beta).py
+++ main_suit(beta).py
@@ -1,20 +1,18 @@
-import random
+import random as rnd
 
-print("=====PERMAINAN BATU GUNTING KERTAS=====\n")
+print("PERMAINAN BATU GUNTING KERTAS".center(50, " "))
 while True:
     player = input('Masukkan pilihan Anda (Batu, Gunting, Kertas) : ')
     if player.lower() != "gunting" and player.lower() != "kertas" and player.lower() != "batu":
         print("Masukkan huruf yang sesuai!")
-
     else:
-        com = random.choice(["Kertas", "Batu", "Gunting"])
+        com = rnd.choice(["Kertas", "Batu", "Gunting"])
         print(f"Anda memilih {player.upper()} Komputer memilih {com.upper()}")
-
         if (player.lower() == "kertas" and com == "Batu") or (player.lower() == "batu" and com == "Gunting") or (player.lower() == "gunting" and com == "Kertas"):
             print("Anda Menang!")
         elif player.lower() == com.lower(): print("Seri!")
         else: print("Anda Kalah")
 
-    play = input("\nIngin main lagi?\nYa / Tidak: ")
+    play = input("\nIngin main lagi?\nYa atau Tidak: ")
     if play.lower() == "tidak": print("Terima kasih sudah bermain!"), exit()
     elif play.lower() != "ya": print("Masukkan pilihan yang sesuai!"), exit()
